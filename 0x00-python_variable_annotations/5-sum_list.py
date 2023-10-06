259f381072adab9a2b8948628c400d9c0f95231e
lawalgodwin <godwinl200@gmail.com>
2023-10-06 15:41:42 +0100

    Add solution to task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""A module that contains a type-annotated function sum_list which
+takes a list input_list of floats as argument and returns their
+sum as a float.
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """Return the sum of all floats given"""
+    sum = 0.0
+    for f in input_list:
+        sum = sum + f
+    return sum

7207ec121f4b039d666121e9ae9a73a1d0564d3a
lawalgodwin <godwinl200@gmail.com>
2023-10-06 14:36:56 +0100

    Add solution to task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A module that contains a type-annotated function concat 2
+strings and returns the reulted string
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """Concat 2 strings and return the resulting string"""
+    return str1 + str2

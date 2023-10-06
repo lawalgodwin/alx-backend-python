01db145607ab2827b838d9e65c320aab08b20a2e
lawalgodwin <godwinl200@gmail.com>
2023-10-06 14:11:36 +0100

    Define a function with annotaion

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A module that contains annotated function"""
+
+
+def add(a: float, b: float) -> float:
+    # Add two floats and returns the resulting float
+    return (a + b)

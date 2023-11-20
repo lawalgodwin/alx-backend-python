b90fbef5e56d5b5f98e170d2c5b7f2e862a53c5f
lawalgodwin <godwinl200@gmail.com>
2023-11-20 12:14:12 +0100

    Mock an instance variable/property

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Test for Client.py module"""
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -20,6 +20,21 @@ class TestGithubOrgClient(unittest.TestCase):
         org_url = 'https://api.github.com/orgs/{}'.format(org_name)
         spec.org()
         mock_get_json.assert_called_once_with(org_url)
+
+    @parameterized.expand([
+        ('google', {"repos_url": "https://api.someurl.com"})
+        ])
+    def test_public_repos_url(self, org_name, exp_repos_url: dict):
+        """Mocking a property and unit-test the method ._public_repos_url
+        Note: memoize automatically turns the decorated nethod into
+        an instance property"""
+        with patch.object(
+            GithubOrgClient, 'org',
+            new=exp_repos_url
+        ):
+            org_client = GithubOrgClient(org_name)
+            real_repos_url = org_client._public_repos_url
+            self.assertEqual(real_repos_url, exp_repos_url.get("repos_url"))
 
 
 if __name__ == '__main__':

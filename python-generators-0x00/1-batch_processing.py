71314aea551bae5c819524a076e19daefed0f8a4
lawalgodwin <godwinl200@gmail.com>
2024-12-04 11:14:24 +0100

    Fetch rows by batch

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -5,11 +5,32 @@
     This module creates a generator to fetch and process data in batches from the users database
 """
 
-def stream_users_in_batches(batch_size):
+from mysql.connector import Error
+from seed import connect_to_prodev
+
+conn = connect_to_prodev()
+
+def stream_users_in_batches(batch_size = 50):
     """ A function that fetches rows in batches
 
         Arguments:
             batch_size: The number of records to be fetched per batch
         Returns: list of records fetched
     """
-    
+    with conn.cursor() as cursor:
+        QUERY = """ SELECT * FROM user_data """
+        cursor.execute(QUERY)
+        resultsets = cursor.fetchmany(batch_size)
+        while resultsets:
+            yield resultsets
+            resultsets = cursor.fetchmany(batch_size)
+            if not resultsets:
+                break
+            yield resultsets
+               
+
+if __name__ == "__main__":
+    try:
+        print(next(stream_users_in_batches(6)))
+    except Error as e:
+        print(e.msg)

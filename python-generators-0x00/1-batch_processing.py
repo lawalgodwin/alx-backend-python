a382055b8ba54d4efd5647fc816c35b7fa9ff519
lawalgodwin <godwinl200@gmail.com>
2024-12-05 15:29:25 +0100

    Fetch rows by batch

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -5,10 +5,10 @@
     This module creates a generator to fetch and process data in batches from the users database
 """
 
-from mysql.connector import Error
-from seed import connect_to_prodev
+from mysql.connector import Error, connect
+# from seed import connect_to_prodev
 
-conn = connect_to_prodev()
+conn = connect(host="0.0.0.0", user="nedu", password="password", database="ALX_prodev")
 
 def stream_users_in_batches(batch_size = 50):
     """ A function that fetches rows in batches
@@ -17,20 +17,20 @@ def stream_users_in_batches(batch_size = 50):
             batch_size: The number of records to be fetched per batch
         Returns: list of records fetched
     """
-    with conn.cursor() as cursor:
-        QUERY = """ SELECT * FROM user_data """
-        cursor.execute(QUERY)
-        resultsets = cursor.fetchmany(batch_size)
-        while resultsets:
-            yield resultsets
-            resultsets = cursor.fetchmany(batch_size)
-            if not resultsets:
-                break
-            yield resultsets
-               
+    cursor = conn.cursor(dictionary=True)
+    QUERY = """ SELECT * FROM user_data """
+    cursor.execute(QUERY)
+    rows =  cursor.fetchmany(batch_size)
+    while True:
+        if not rows:
+            break
+        yield rows
+        rows = cursor.fetchmany(batch_size)
+    return "No more rows to fetch"
+
 
 if __name__ == "__main__":
     try:
-        print(next(stream_users_in_batches(6)))
+        print(next(stream_users_in_batches(2)))
     except Error as e:
-        print(e.msg)
+        print(e.value)

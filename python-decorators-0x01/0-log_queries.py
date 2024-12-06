b989d4ab9b7914eb2950f7384a06d47a6b8f7e85
lawalgodwin <godwinl200@gmail.com>
2024-12-06 02:29:31 +0100

    Implement a decorator that logs SQL queries before executing it

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+""" Logging database Queries
+    This module cotains a decorator that logs all SQL queries run by the decorated function
+    Objective: create a decorator that logs database queries executed by any function
+"""
+
+import sqlite3
+import functools
+
+
+def create_table():
+    """ creates a table users if it does not exists with the required fields
+        Arguments:
+            connection: The connection object
+        Returns: None
+    """
+    connection = sqlite3.connect('users.db')
+    sqlite_create_table_query = '''CREATE TABLE IF NOT EXISTS users (
+                                user_id INTEGER PRIMARY KEY,
+                                name TEXT NOT NULL,
+                                email TEXT NOT NULL UNIQUE,
+                                joining_date DATETIME,
+                                salary REAL NOT NULL);'''
+    cursor = connection.cursor()
+    cursor.execute(sqlite_create_table_query)
+
+# cretae the users table
+create_table()
+
+#### decorator to log SQL queries
+
+def log_queries(fn):
+    """ A decorator that logs database queries before executing it """
+    @functools.wraps(fn)
+    def wrapper(query):
+        print(query)
+        fn(query)
+    return wrapper
+        
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
+

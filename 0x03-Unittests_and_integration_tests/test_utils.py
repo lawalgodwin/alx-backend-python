e1965c4487f9731f33aaa4f37677aaf5b7595ae2
lawalgodwin <godwinl200@gmail.com>
2024-12-10 16:06:35 +0100

    Perform test for Exception(KeyError)

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,7 +8,7 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """ A test case for the nested_map function """
+    """ A test case for the nested_map function  """
 
     @parameterized.expand([
             ({"a": 1}, ("a",), 1),
@@ -19,6 +19,15 @@ class TestAccessNestedMap(unittest.TestCase):
         """ A method that tests for expected outputs """
         actual_result = access_nested_map(nested_map, path)
         self.assertEqual(actual_result, expected)
+
+    @parameterized.expand([
+        ({}, ("a",), ),
+        ({"a": 1}, ("a", "b"))
+    ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """ Test for exception(KeyError) """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
 
 
 if __name__ == "__main__":

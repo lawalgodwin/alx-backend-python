8595b7cf9910d867ef7e111b35506cc6a3da916b
lawalgodwin <godwinl200@gmail.com>
2024-12-12 01:05:47 +0100

    Test a memoized method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@ import parameterized
 from parameterized import parameterized
 import unittest
 from unittest.mock import patch, MagicMock, Mock
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 import utils
 
 
@@ -50,6 +50,29 @@ class TestGetJson(unittest.TestCase):
             mock_requests_get.assert_called_once_with(url)
             # Test that the output of get_json is equal to test_payload
             self.assertDictEqual(actual, expected)
+
+
+class TestMemoize(unittest.TestCase):
+    """ Test for memoization method """
+    def test_memoize(self):
+        """ Test that when calling a_property twice, the correct result is
+        returned but a_method is only called once
+        """
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, "a_method") as mock_a_method:
+            myclass = TestClass()
+            result1 = myclass.a_property()
+            result2 = myclass.a_property()
+            # Test that the a_method was called only once due to memoization
+            mock_a_method.assert_called_once()
+            self.assertEqual(result1, result2)
 
 
 if __name__ == "__main__":

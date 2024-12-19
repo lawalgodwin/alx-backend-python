83b4c5d36e3f18d1d247c4bbdee0ef4159b18d5d
lawalgodwin <godwinl200@gmail.com>
2024-12-19 15:32:22 +0100

    Create serializers to define the many-to-many relationships

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -4,6 +4,12 @@ from .models import User, Conversation, Message
 
 
 class UserSerializer(serializers.ModelSerializer):
+    conversations = serializers.SerializerMethodField(method_name="user_conversations")
+
+    def user_conversations(self, obj):
+        return obj.conversations
+
+
     class Meta:
         model = User
         exclude = ("created_at", "password_hash",)

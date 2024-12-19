dd89d909b129a7d9d2eb75a5265d19b54ed1fcce
lawalgodwin <godwinl200@gmail.com>
2024-12-19 15:06:27 +0100

    Define data models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -27,18 +27,19 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ["first_name", "last_name", "password_hash", "phone_number"]
 
 
+class Conversation(models.Model):
+    """  The conversation model tracks which users are involved in a conversation """
+    conversation_id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    participants_id = models.ManyToManyField(User, related_name="conversations")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
 class Message(models.Model):
     """ The message model containing the sender, conversation as
         defined in the shared schema attached to this project
     """
     message_id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    sender_id = models.OneToOneField(User, on_delete=models.CASCADE, related_name="sender")
+    sender_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    conversation_id = models.ForeignKey(Conversation, default=None, on_delete=models.CASCADE, related_name="messages")
     message_body = models.TextField()
     sent_at = models.DateTimeField(auto_now_add=True)
-
-
-class Conversation(models.Model):
-    """  The conversation model tracks which users are involved in a conversation """
-    conversation_id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    participants_id = models.ForeignKey(User, related_name="participants", on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)

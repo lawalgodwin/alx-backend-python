392120cf61cd5c54dfffc67013334198b0a29a3e
lawalgodwin <godwinl200@gmail.com>
2024-12-22 09:08:29 +0100

    Set up url routing

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,12 +1,31 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
 
+router = routers.DefaultRouter()
+router.register(
+    "conversations",
+    views.ConversationViewSet,
+    basename="conversations"
+)
+
+router.register(
+    "messages",
+    views.MessageViewSet,
+    basename="messages"
+)
+
+
+# urlpatterns = [
+#     path("conversations/", views.ConversationViewSet.as_view({ "post": "create", "get": "list" }), name="conversations"),
+#     path("conversations/pk", views.ConversationViewSet.as_view({"get": "retrieve"}), name="conversations"),
+#     path("messages/", views.MessageViewSet.as_view({ "post": "create",  "get": "list" }), name="messages"),
+#     path("messages/pk", views.MessageViewSet.as_view({"get": "retrieve"}), name="messages"),
+# ]
+
 urlpatterns = [
-    path("conversations/", views.ConversationViewSet.as_view({ "post": "create", "get": "list" }), name="conversations"),
-    path("conversations/pk", views.ConversationViewSet.as_view({"get": "retrieve"}), name="conversations"),
-    path("messages/", views.MessageViewSet.as_view({ "post": "create",  "get": "list" }), name="messages"),
-    path("messages/pk", views.MessageViewSet.as_view({"get": "retrieve"}), name="messages"),
+    path("", include(router.urls))
 ]
 
 # conversation_create = views.ConversationViewSet.as_view({"post": "create"})
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -6,49 +6,53 @@ from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
 
-class ConversationViewSet(viewsets.ViewSet):
+class ConversationViewSet(viewsets.ModelViewSet):
     """ A viewset for listing and performing CRUD operation
         on the conversation model
     """
-    def list(self, request):
-        conversations = Conversation.objects.all()
-        serializer = ConversationSerializer(conversations, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    # def list(self, request):
+    #     conversations = Conversation.objects.all()
+    #     serializer = ConversationSerializer(conversations, many=True)
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
     
-    def retrieve(self, request, pk=None):
-        queryset = Conversation.objects.all()
-        conversation = get_list_or_404(queryset, pk=pk)
-        serializer = ConversationSerializer(conversation)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    # def retrieve(self, request, pk=None):
+    #     queryset = Conversation.objects.all()
+    #     conversation = get_list_or_404(queryset, pk=pk)
+    #     serializer = ConversationSerializer(conversation)
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
     
-    def create(self, request):
-        data = request.data
-        serializer = ConversationSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def create(self, request):
+    #     data = request.data
+    #     serializer = ConversationSerializer(data=data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class MessageViewSet(viewsets.ViewSet):
+class MessageViewSet(viewsets.ModelViewSet):
     """ A viewset for listing and performing CRUD operation
         on the message model
     """
-    def list(self, request):
-        messages = Message.objects.all()
-        serializer = MessageSerializer(messages, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    # def list(self, request):
+    #     messages = Message.objects.all()
+    #     serializer = MessageSerializer(messages, many=True)
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
     
-    def create(self, request):
-        data = request.data
-        serializer = MessageSerializer(data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # def create(self, request):
+    #     data = request.data
+    #     serializer = MessageSerializer(data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
+    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def retrieve(self, request, pk=None):
-        queryset = Message.objects.all()
-        message = get_list_or_404(queryset, pk=pk)
-        serializer = MessageSerializer(message)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+    # def retrieve(self, request, pk=None):
+    #     queryset = Message.objects.all()
+    #     message = get_list_or_404(queryset, pk=pk)
+    #     serializer = MessageSerializer(message)
+    #     return Response(serializer.data, status=status.HTTP_200_OK)

252b4d908d51aa19c23eb923707730a26a82a7a6
lawalgodwin <godwinl200@gmail.com>
2025-01-14 22:51:40 +0100

    implement a  middleware that restricts access to the messaging app during certain hours of the day

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from datetime import datetime
+from django.http.response import HttpResponseForbidden
 
 class RequestLoggingMiddleware:
     """A middleware that logs each user’s requests to a file, 
@@ -16,4 +17,21 @@ class RequestLoggingMiddleware:
             log_file.write(log_message)
         response = self.get_response(request)
         # manipulate the response object before it reaches the client
+        return response
+
+
+class RestrictAccessByTimeMiddleware:
+    """A middleware that restricts access to the
+    messaging app during certain hours of the day
+    """
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        server_current_hour = datetime.now().hour
+        if (server_current_hour < 9) or (server_current_hour >= 18):
+            # access denied for access outside 9am - 6pm
+            return HttpResponseForbidden("Access denied....Try again between 9am-6pm")
+        response = self.get_response(request)
+        pass
         return response

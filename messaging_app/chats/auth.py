f6b5de52c7a476729436fea2f8bd801df1544c31
lawalgodwin <godwinl200@gmail.com>
2025-01-20 23:58:14 +0100

    Dockerize the message app with connection to sqlite3 db

--- messaging_app/chats/auth.py
+++ messaging_app/chats/auth.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 from .serializers import UserCreateSerializer
 
 
 class UserRegisterViewSet(generics.CreateAPIView):
     """User registration view"""
     serializer_class = UserCreateSerializer
+    permission_classes = [AllowAny]
